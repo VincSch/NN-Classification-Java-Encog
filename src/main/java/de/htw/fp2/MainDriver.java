c08c67688bf5d492193aa03a70867df48d2be98d
VincSch <vs@stroodel.com>
2014-12-09 12:22:49 +0100

    initial maven project with dependencies

---
+++ src/main/java/de/htw/fp2/MainDriver.java
@@ -0,0 +1,24 @@
+/*
+ *
+ * Copyright (C) 2014
+ *
+ */
+
+package de.htw.fp2;
+
+import org.apache.log4j.Logger;
+
+public class MainDriver {
+
+    private static Logger log = Logger.getLogger(MainDriver.class.getName());
+
+    /**
+     * Main method.
+     *
+     * @param args the args
+     */
+    public static void main(final String[] args) {
+
+        log.info("Hello from MainDriver");
+    }
+}
